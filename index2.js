e84f5a53b607e21b8eeb496e5d40601549e3e851
Yulia Kovylina <yulianastepanovaa@yandex.ru>
2021-10-17 16:26:29 +0300

    Сделать модальное окно Корзина, Продуктовая карта 2

--- index2.js
+++ index2.js
@@ -5,6 +5,7 @@ let cardsPizza = [
             name: 'Изображение',
         },
         title: 'Пицца из половинок',
+        subtitle: '30 см, традиционное тесто, 480 г',
         description: 'Соберите свою пиццу 35 см с двумя разными вкусами',
         cost: 'от 600 &#8381;',
         btn: 'Собрать',
@@ -17,6 +18,7 @@ let cardsPizza = [
             name: 'Изображение',
         },
         title: 'Пепперони-сердце',
+        subtitle: '30 см, традиционное тесто, 480 г',
         description: 'Пикантная пепперони, моцарелла, томатный соус',
         cost: 'от 625 &#8381;',
         btn: 'Выбрать',
@@ -29,6 +31,7 @@ let cardsPizza = [
             name: 'Изображение',
         },
         title: 'Пирог-сердце',
+        subtitle: '30 см, традиционное тесто, 480 г',
         description: 'Ананасы, брусника, сгущенное молоко',
         cost: 'от 625 &#8381;',
         btn: 'Выбрать',
@@ -41,6 +44,7 @@ let cardsPizza = [
             name: 'Изображение',
         },
         title: 'Чиззи чеддер',
+        subtitle: '30 см, традиционное тесто, 480 г',
         description: 'Ветчина, сыр чеддер, сладкий перец, моцарелла, томатный соус, чеснок, итальянские травы',
         cost: 'от 625 &#8381;',
         btn: 'Выбрать',
@@ -53,6 +57,7 @@ let cardsPizza = [
             name: 'Изображение',
         },
         title: 'Цыпленок блю чиз',
+        subtitle: '30 см, традиционное тесто, 480 г',
         description: 'Цыпленок, сыр блю чиз, томаты, моцарелла, соус альфредо',
         cost: 'от 445 &#8381;',
         btn: 'Выбрать',
@@ -65,6 +70,7 @@ let cardsPizza = [
             name: 'Изображение',
         },
         title: 'Нежный лосось',
+        subtitle: '30 см, традиционное тесто, 480 г',
         description: 'Лосось, томаты черри, соус песто, моцарелла, соус альфредо',
         cost: 'от 495 &#8381;',
         btn: 'Выбрать',
@@ -77,6 +83,7 @@ let cardsPizza = [
             name: 'Изображение',
         },
         title: 'Сырная',
+        subtitle: '30 см, традиционное тесто, 480 г',
         description: 'Увеличенная порция моцареллы, сыры чеддер и пармезан, соус альфредо',
         cost: 'от 245 &#8381;',
         btn: 'Выбрать',
@@ -89,6 +96,7 @@ let cardsPizza = [
             name: 'Изображение',
         },
         title: 'Пепперони фреш',
+        subtitle: '30 см, традиционное тесто, 480 г',
         description: 'Пикантная пепперони,<br> увеличенная порция моцареллы, томаты, томатный соус',
         cost: 'от 645 &#8381;',
         btn: 'Выбрать',
@@ -98,6 +106,7 @@ let cardsPizza = [
 
 let cardsCombo = [
     {
+        id: 'Combo01',
         img: {
             src: './images/combo.jpg',
             name: 'Изображение',
@@ -109,6 +118,7 @@ let cardsCombo = [
         btnMobile: '599 &#8381;',
     },
     {
+        id: 'Combo02',
         img: {
             src: './images/combo.jpg',
             name: 'Изображение',
@@ -120,6 +130,7 @@ let cardsCombo = [
         btnMobile: '899 &#8381;',
     },
     {
+        id: 'Combo03',
         img: {
             src: './images/combo.jpg',
             name: 'Изображение',
@@ -131,6 +142,7 @@ let cardsCombo = [
         btnMobile: '749 &#8381;',
     },
     {
+        id: 'Combo04',
         img: {
             src: './images/combo.jpg',
             name: 'Изображение',
@@ -142,6 +154,7 @@ let cardsCombo = [
         btnMobile: '449 &#8381;',
     },
     {
+        id: 'Combo05',
         img: {
             src: './images/combo.jpg',
             name: 'Изображение',
@@ -156,6 +169,7 @@ let cardsCombo = [
 
 let cardsStarters = [
     {
+        id: 'Starters01',
         img: {
             src: './images/starters/soup.jpg',
             name: 'Изображение',
@@ -167,6 +181,7 @@ let cardsStarters = [
         btnMobile: '99 &#8381;',
     },
     {
+        id: 'Starters02',
         img: {
             src: './images/starters/soup.jpg',
             name: 'Изображение',
@@ -178,6 +193,7 @@ let cardsStarters = [
         btnMobile: '149 &#8381;',
     },
     {
+        id: 'Starters03',
         img: {
             src: './images/starters/dodster.jpg',
             name: 'Изображение',
@@ -189,6 +205,7 @@ let cardsStarters = [
         btnMobile: '159 &#8381;',
     },
     {
+        id: 'Starters04',
         img: {
             src: './images/starters/dodster-hot.jpg',
             name: 'Изображение',
@@ -200,6 +217,7 @@ let cardsStarters = [
         btnMobile: '159 &#8381;',
     },
     {
+        id: 'Starters05',
         img: {
             src: './images/starters/dodster-chipotle.jpg',
             name: 'Изображение',
@@ -214,6 +232,7 @@ let cardsStarters = [
 
 let cardDeserts = [
     {
+        id: 'Deserts01',
         img: {
             src: './images/desserts/apple-pie.jpg',
             name: 'Изображение',
@@ -225,6 +244,7 @@ let cardDeserts = [
         btnMobile: '99 &#8381;',
     },
     {
+        id: 'Deserts02',
         img: {
             src: './images/desserts/cheesecake.jpg',
             name: 'Изображение',
@@ -236,6 +256,7 @@ let cardDeserts = [
         btnMobile: '149 &#8381;',
     },
     {
+        id: 'Deserts03',
         img: {
             src: './images/desserts/fondan.jpg',
             name: 'Изображение',
@@ -247,6 +268,7 @@ let cardDeserts = [
         btnMobile: '159 &#8381;',
     },
     {
+        id: 'Deserts04',
         img: {
             src: './images/desserts/milkshake.jpg',
             name: 'Изображение',
@@ -258,6 +280,7 @@ let cardDeserts = [
         btnMobile: '159 &#8381;',
     },
     {
+        id: 'Deserts05',
         img: {
             src: './images/desserts/milkshake.jpg',
             name: 'Изображение',
@@ -272,6 +295,7 @@ let cardDeserts = [
 
 let cardsDrinks = [
     {
+        id: 'Drinks01',
         img: {
             src: './images/drinks/1.jpg',
             name: 'Изображение',
@@ -283,6 +307,7 @@ let cardsDrinks = [
         btnMobile: '99 &#8381;',
     },
     {
+        id: 'Drinks01',
         img: {
             src: './images/drinks/2.jpg',
             name: 'Изображение',
@@ -294,6 +319,7 @@ let cardsDrinks = [
         btnMobile: '149 &#8381;',
     },
     {
+        id: 'Drinks01',
         img: {
             src: './images/drinks/3.jpg',
             name: 'Изображение',
@@ -305,6 +331,7 @@ let cardsDrinks = [
         btnMobile: '159 &#8381;',
     },
     {
+        id: 'Drinks01',
         img: {
             src: './images/drinks/4.jpg',
             name: 'Изображение',
@@ -316,6 +343,7 @@ let cardsDrinks = [
         btnMobile: '159 &#8381;',
     },
     {
+        id: 'Drinks01',
         img: {
             src: './images/drinks/5.jpg',
             name: 'Изображение',
@@ -327,12 +355,16 @@ let cardsDrinks = [
         btnMobile: '159 &#8381;',
     },
 ]
-function createCard(massive, massiveId, classLength) {
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////   CREATE CARD   //////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+function createCard(massive, massiveId, classLength, ) {
     let el = document.createElement('div')
     el.className = 'main-cards'
-    //let el = document.createElement('div')
     for (let i = 0; i < massive.length; i++){
-        let card = `
+        el.innerHTML += `
     <div class="main-card ${classLength}">
          <div class="main-card__image-wrapper">
             <img class="main-card__image" src="${massive[i].img.src}">
@@ -342,41 +374,105 @@ function createCard(massive, massiveId, classLength) {
             <p class="main-text_small_14_gray main-card__text_small-width">${massive[i].description}</p>
             <div class="main-card__bottom">
                 <p class="main-card__text_16">${massive[i].cost}</p>
-                <button type="button" class="main-button">${massive[i].btn}</button>
+                <button id="${massive[i].id}" type="button" class="main-button" onclick="openModal(event)">${massive[i].btn}</button>
             </div>
-            <button type="button" class="main-button main-button_light button-price">${massive[i].btnMobile}</button>
+            <button id="${massive[i].id}" type="button" class="main-button main-button_light button-price">${massive[i].btnMobile}</button>
          </div>
      </div>
     `
-       // let el = document.createElement('div')
-       // let el = document.getElementsByClassName('main-cards')
-        el.innerHTML += card
-
     }
     document.getElementById(massiveId).append(el)
-    for (let i = 0; i < massive.length; i++ ) {
-        let basketButton = el.querySelector('.main-button')
-        basketButton.addEventListener('click', openModal)
-    }
+}
+
+function createModal(massive) {
+    let id = massive.id
+    console.log(id)
+    let modalWrapper = document.createElement('div')
+    modalWrapper.className = 'product-card'
+    modalWrapper.innerHTML = `
+        <div class="image__container">
+            <img class="product-card__image" src='${id.img.src}'>
+            <img class="product-card__outline" src="images/product-card/Ellipse%206.png">
+        </div>
+        <div class="main-container">
+            <h1 class="product-card__title">${id.title}</h1>
+            <p class="product-card__description">${id.subtitle}</p>
+            <div class="product-card__adds">
+            <p class="product-card__add">${id.description}</p>
+<!--            <ul class="product-card__adds">-->
+<!--                <li class="product-card__add product-card__add_underlined">Ветчина <img class="product-card__add_delete" src='./images/product-card/delete.svg'>, </li>-->
+<!--                <li class="product-card__add"> сыр чеддер, </li>-->
+<!--                <li class="product-card__add product-card__add_underlined"> сладкий перец <img class="product-card__add_delete" src='./images/product-card/delete.svg'>, </li>-->
+<!--                <li class="product-card__add"> моцарелла, </li>-->
+<!--                <li class="product-card__add"> томатный соус, </li>-->
+<!--                <li class="product-card__add product-card__add_crossed-out"> чеснок <img class="product-card__add_back" src='./images/product-card/back.svg'>, </li>-->
+<!--                <li class="product-card__add product-card__add_underlined"> итальянские травы <img class="product-card__add_delete" src='./images/product-card/delete.svg'> </li>-->
+<!--            </ul>-->
+            <div>
+            <div class="size-container">
+                <button class="size__button size__button_M">Маленькая</button>
+                <button class="size__button size__button_M size__button_clicked">Средняя</button>
+                <button class="size__button size__button_M">Большая</button>
+            </div>
+            <div class="size-container">
+                <button class="size__button size__button_L">Традиционное</button>
+                <button class="size__button size__button_L size__button_clicked">Тонкое</button>
+            </div>
+            <h2 class="adds__title">Добавить в пиццу</h2>
+            <div class="ingridients">
+                <div class="ingridient-item">
+                    <img class="ingridients-item__image" src="./images/product-card/cheese.jpg">
+                    <h3 class="ingridients-item__title">Сырный бортик</h3>
+                    <p class="ingridients-item__text">599 &#8381;</p>
+                </div>
+                <div class="ingridient-item">
+                    <img class="ingridients-item__image" src="./images/product-card/champinion.jpg">
+                    <h3 class="ingridients-item__title">Шампиньоны</h3>
+                    <p class="ingridients-item__text">39 &#8381;</p>
+                </div>
+                <div class="ingridient-item">
+                    <img class="ingridients-item__image" src="./images/product-card/chicken.jpg">
+                    <h3 class="ingridients-item__title">Цыпленок</h3>
+                    <p class="ingridients-item__text">59 &#8381;</p>
+                </div>
+                <div class="ingridient-item">
+                    <img class="ingridients-item__image" src="./images/product-card/brynza.jpg">
+                    <h3 class="ingridients-item__title">Брынза</h3>
+                    <p class="ingridients-item__text">59 &#8381;</p>
+                </div>
+                <div class="ingridient-item">
+                    <img class="ingridients-item__image" src="./images/product-card/halopeniy.jpg">
+                    <h3 class="ingridients-item__title">Острый халопенью</h3>
+                    <p class="ingridients-item__text">39 &#8381;</p>
+                </div>
+                <div class="ingridient-item">
+                    <img class="ingridients-item__image" src="./images/product-card/hot_chicken.jpg">
+                    <h3 class="ingridients-item__title">Острый цыпленок</h3>
+                    <p class="ingridients-item__text">59 &#8381;</p>
+                </div>
+            </div>
+            <button class="product-card__button">Добавить в корзину за 625 &#8381;</button>
+            <div class="close-container">
+                <img src="./images/product-card/close.svg" class="close-button" onclick="closeModal()">
+            </div>
+        </div>       
+       `
+    document.querySelector('.product-card__container').append(modalWrapper)
 }
 
 
-let basket = document.querySelector('.main-button')
 let modal = document.querySelector('.product-card__container')
-let closeButton = modal.querySelector('.close-button')
 
-function openModal() {
-    let image = modal.querySelector('.product-card__image')
-    image.src = cardsPizza[1].img.src
+function openModal(event) {
+    console.log(event.target.id)
     modal.classList.remove('product-card__container_hidden')
-    console.log(modal)
 }
-basket.addEventListener('click', openModal)
 
 function  closeModal() {
     modal.classList.add('product-card__container_hidden')
 }
-closeButton.addEventListener('click', closeModal)
+
+// closeButton.addEventListener('click', closeModal)
 
 createCard(cardsPizza, 'pizza-cards', 'main-card_size-S')
 createCard(cardsCombo, 'combo-cards', 'main-card_size-S')
